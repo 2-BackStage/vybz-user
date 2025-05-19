ce00b972379ec733f903b8349f9c942adea85410
HyeonProG <guswo3443@naver.com>
2025-05-19 16:56:16 +0900

    fix: fanInfo generationType 수정

--- src/main/java/back/vybz/userservice/user/domain/mysql/FanInfo.java
+++ src/main/java/back/vybz/userservice/user/domain/mysql/FanInfo.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class FanInfo extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "user_uuid", nullable = false, unique = true)
