fb8265b5fcaed49a5691880e95d883d5166ce4db
HyeonProG <guswo3443@naver.com>
2025-05-28 10:38:35 +0900

    refactor: 유저 프로필 이미지 update 매개변수 수정

--- src/main/java/back/vybz/userservice/user/presentation/UserInfoController.java
+++ src/main/java/back/vybz/userservice/user/presentation/UserInfoController.java
@@ -9,6 +9,7 @@ import back.vybz.userservice.user.dto.request.RequestUpdateUserInfoDto;
 import back.vybz.userservice.user.dto.response.ResponseUserInfoDto;
 import back.vybz.userservice.user.vo.request.RequestAddUserInfoVo;
 import back.vybz.userservice.user.vo.request.RequestDeleteUserInfoVo;
+import back.vybz.userservice.user.vo.request.RequestUpdateUserInfoVo;
 import back.vybz.userservice.user.vo.response.ResponseUserInfoVo;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -62,16 +63,12 @@ public class UserInfoController {
 
     /**
      * 유저 정보 수정
-     * @param userUuid
-     * @param profileImage
-     * @param nickname
+     * @param requestUpdateUserInfoVo
      */
     @Operation(summary = "유저 정보 수정 API", description = "유저 정보 수정 API 입니다.", tags = {"User-Service"})
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public BaseResponseEntity<Void> updateUserInfo(@RequestPart("userUuid") String userUuid,
-                                                   @RequestPart(value = "profileImage", required = false) MultipartFile profileImage,
-                                                   @RequestPart("nickname") String nickname) {
-        userInfoService.updateUserInfo(RequestUpdateUserInfoDto.of(userUuid, profileImage, nickname));
+    public BaseResponseEntity<Void> updateUserInfo(@ModelAttribute RequestUpdateUserInfoVo requestUpdateUserInfoVo) {
+        userInfoService.updateUserInfo(RequestUpdateUserInfoDto.from(requestUpdateUserInfoVo));
         return new BaseResponseEntity<>(BaseResponseStatus.SUCCESS);
     }
 
