e6899504e72da9695e8358cd6f419ce30db667aa
HyeonProG <167273902+HyeonProG@users.noreply.github.com>
2025-06-04 19:54:12 +0900

    chore: mongodb gradle 제거 (#41)

--- src/main/java/back/vybz/user_info_service/common/config/MongoConfig.java
+++
@@ -1,9 +0,0 @@
-package back.vybz.user_info_service.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-
-@Configuration
-@EnableMongoAuditing
-public class MongoConfig {
-}
