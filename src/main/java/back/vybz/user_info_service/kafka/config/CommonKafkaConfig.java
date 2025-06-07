58abd19c0cc4178b795f5e5646e77b2075d3a7d7
HyeonProG <guswo3443@naver.com>
2025-06-08 00:51:56 +0900

    refactor: 유저 정보 kafka consume 리팩토링

--- src/main/java/back/vybz/user_info_service/kafka/config/CommonKafkaConfig.java
+++ src/main/java/back/vybz/user_info_service/kafka/config/CommonKafkaConfig.java
@@ -27,7 +27,6 @@ public class CommonKafkaConfig {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, UserInfoEvent.class);
 
         return props;
     }
